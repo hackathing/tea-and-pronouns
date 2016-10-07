a8f13c115c6699d34d6b5644679664f9a5da50a1
Louis Pilfold <louis@lpil.uk>
2016-10-07 16:15:47 +0100

    App baby React app

--- client/index.js
+++ client/index.js
@@ -1,1 +1,10 @@
-console.log("Hello, world!");
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const element = document.querySelector('.js-app');
+
+const App = () => (
+  <h1>Tea & Pronouns!</h1>
+);
+
+ReactDOM.render(<App />, element);
